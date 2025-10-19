471bb6e2521682ea590947fdbf62fb56d4d892bb
James <james.ralph8555@gmail.com>
2025-10-18 20:31:56 -0400

    python313

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+        pythonEnv = pkgs.python313.withPackages (ps: with ps; [
           pandas
           yfinance
           numpy
